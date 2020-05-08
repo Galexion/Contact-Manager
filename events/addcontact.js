d43f67fa163d69c6f6e63abe4f2864fc6bc840a1
Tails Era <TailsEraYT@hostingwebsites.tk>
2020-05-08 18:08:35 -0400

    Escargot Manager Live

---
+++ events/addcontact.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const embed = {
+  "color": 16777215,
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/attachments/617442660038344803/617560922814414848/Webp.net-resizeimage.jpg",
+    "text": "In Partnership With EraMedia || Escargot Manager"
+  },
+  "fields": [
+    {
+      "name": "Adding Contacts into the System Beta",
+      "value": "========"
+    },
+    {
+      "name": "Adding Though DM's",
+      "value": "Adding though DM's is Simple. all you need is the WLM / MSNM CLient. do !website for information."
+    },
+    {
+      "name": "Contacting",
+      "value": "please forward all requests to TailsEraYT@gmail.com THOUGH the WLM / MSNM Client. Email Requests will not be accepted."
+    },
+    {
+        "name": "Publishing",
+        "value": "Allow A Alloted Time of about 1 - 24 hours for our Developers to Publish your Information to the Bot. We can't Wait to see you on the Service."
+    }
+  ]
+};
+
+module.exports.embed = embed
