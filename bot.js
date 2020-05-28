2e02d6eaa36b018730113978c98ad79e05319f7a
Tails Era <TailsEraYT@hostingwebsites.tk>
2020-05-27 23:00:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,19 +4,13 @@ const client = new Discord.Client();
 //events
 const help = require("./events/help");
 const contacts = {
-    TailsEraYT: require("./events/Contacts/TailsEraYT"),
-    Naidru: require("./events/Contacts/naidru"),
-    sprtcrnbry: require("./events/Contacts/sprtcrnbry"),
-    warlordoverdriv: require("./events/Contacts/warlordoverdriv"),
+//This is where you add contacts
     list: {
-        page1: require("./events/Contacts/List/page1"),
+        page1: require("/*Add a List Dir here*\"),
     }
 };
-const Invite = require("./events/invite");
 const prefix = "!";
-const add = require("./events/addcontact");
 const about = require("./events/about");
-const findout = require("./events/findout");
 
 client.on("ready", () => {
     console.log("Escargot Manager Has Been Launched.");
@@ -34,21 +28,6 @@ client.on("message", message => {
         return message.channel.send({ embed: help.embed }).catch(console.error);
     }
 
-    if (command === "tailserayt") {
-        message.channel.send("> This Command will be gone by 5/25/20. do !findout to find out more.")
-        return message.channel.send({ embed: contacts.TailsEraYT.embed }).catch(console.error);
-    }
-    
-    if (command === "naidru") {
-        message.channel.send("> This Command will be gone by 5/25/20. do !findout to find out more.")
-        return message.channel.send({ embed: contacts.Naidru.embed });
-    }
-    
-     if (command === "sprtcrnbry") {
-         message.channel.send("> This Command will be gone by 5/25/20. do !findout to find out more.")
-        return message.channel.send({ embed: contacts.sprtcrnbry.embed });
-    }
-
     if (command === "add") {
         return message.channel.send({ embed: add.embed });
     }
@@ -58,15 +37,6 @@ client.on("message", message => {
     }
 
     //Commands Code
-    if (command === "website") {
-        message.author.send("> Can't Wait to Message to your friends? Download Escargot @ https://escargot.log1p.xyz !");
-        return message.channel.send("Check your DM's.");
-    }
-
-    if (command === "invite") {
-        message.delete().catch(console.error);
-        return message.author.send({ embed: Invite.embed }).catch(() => {});
-    }
 
     if (command === "about") {
         message.delete().catch(console.error);
@@ -77,24 +47,18 @@ client.on("message", message => {
         if (args.length < 1) return message.channel.send("> Usage:\n> !contact <contact/list> <list page #>");
         
         switch (args[0]) {
-            //contacts first
-            case "TailsEraYT":
-            return message.channel.send({ embed: contacts.TailsEraYT.embed });
-            case "naidru":
-                return message.channel.send({ embed: contacts.Naidru.embed });
-            case "sprtcrnbry":
-                return message.channel.send({ embed: contacts.sprtcrnbry.embed });
-            case "warlordoverdriv":
-                return message.channel.send({ embed: contacts.warlordoverdriv.embed });
-            case "add":
-                return message.channel.send({ embed: add.embed });
-          case "list":
+            //Contacts are added here, With Case being the Command that calls it.
+                
+            //case "TailsEraYT":
+            //return message.channel.send({ embed: contacts.TailsEraYT.embed });
+                
+            case "list":
             if (args.length > 1) {
               switch (args[1]) {
                 case "1":
                   return message.channel.send({ embed: list.page1.embed });
                 default:
-                  //deal with cases where a page that doesnt exist is given
+                  //This Deals a case where no Contact or List Was Given.
                   break;
               }
             } else {
@@ -115,6 +79,6 @@ client.on("message", message => {
 
 //ignore
 
-// FORK NOTE: Your Bot's Token. Available on https://discordapp.com/developers/applications/me
-// TIP: Put it in the .env file.
+// NOTE: Your Bot's Token. Available on https://discordapp.com/developers/applications/me
+// TIP: Put it in the .env file, or if your using heroku, App, then Options, then click ENV.
 client.login(process.env.token);
